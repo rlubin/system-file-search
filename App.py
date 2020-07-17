104bdafd79ea218640cd1c575b8491a722e86347
Ryan Lubin <ryan.lubin1@gmail.com>
2020-07-16 21:11:04 -0400

    added enter hotkey to app, improved ui, still can't update listbox in real time><

--- App.py
+++ App.py
@@ -3,26 +3,26 @@ from tkinter import *
 import re
 import os
 
-def find_matches(path, matches, regex):
-    """recursive function that crawls through directories and appends filenames to matches array that match the regular expression"""
-    for x in os.listdir(path):
-        p = os.path.join(path, x)
-        if os.path.isdir(p):
-            try:
-                a = find_matches(p, matches, regex)
-                paths.extend(a)
-            except:
-                pass
-        if os.path.isfile(p):
-            if re.search(regex, x):
-                matches.append(path + '\\' + x)
+# def find_matches(path, matches, regex):
+#     """recursive function that crawls through directories and appends filenames to matches array that match the regular expression"""
+#     for x in os.listdir(path):
+#         p = os.path.join(path, x)
+#         if os.path.isdir(p):
+#             try:
+#                 a = find_matches(p, matches, regex)
+#                 paths.extend(a)
+#             except:
+#                 pass
+#         if os.path.isfile(p):
+#             if re.search(regex, x):
+#                 matches.append(path + '\\' + x)
 
-def system_search(matches, regex):
-    """populate matches array with regex filename matches"""
-    root_dir = Path.home().parts[0]
-    p = Path('.')
-    p = p.resolve()
-    find_matches(root_dir, matches, regex)
+# def system_search(matches, regex):
+#     """populate matches array with regex filename matches"""
+#     root_dir = Path.home().parts[0]
+#     p = Path('.')
+#     p = p.resolve()
+#     find_matches(root_dir, matches, regex)
 
 def find_matches2(path, listbox, regex):
     """recursive function that crawls through directories and appends filenames to listbox that match the regular expression"""
@@ -37,8 +37,8 @@ def find_matches2(path, listbox, regex):
         if os.path.isfile(p):
             if re.search(regex, x):
                 print(path + '\\' + x)
-                # matches.append(path + '\\' + x)
                 listbox.insert(END, path + '\\' + x)
+                listbox.config()
 
 def system_search2(listbox, regex):
     """populate listbox with filename matches"""
@@ -47,29 +47,22 @@ def system_search2(listbox, regex):
     p = p.resolve()
     find_matches2(root_dir, listbox, regex)
 
-    # WORKS
-    # test = []
-    # for x in range(100):
-    #     test.append(x)
-    # for item in test:
-    #     listbox.insert(END, item)
-
 def search_button():
     """handle search button click, open new window displaying matches"""
     regex = entry.get()
     match_window = Toplevel(root)
     match_window.title(regex)
-    match_window.geometry("500x500")
-    listbox = Listbox(match_window)
+    match_window.geometry('500x500')
+    listbox = Listbox(match_window, selectmode=EXTENDED)
     
     # matches = []
     # system_search(matches, regex)
-    system_search2(listbox, regex)
-    match_num = f'Matches: {listbox.size()}'
-    num_matches = Label(match_window, text=match_num)
-    # label.pack()
+    # system_search2(listbox, regex)
+    matches_label = Label(match_window, text='Matches: ')
+    count_label = Label(match_window, text=listbox.size())
 
-    num_matches.pack(side=TOP, anchor=NW)
+    matches_label.pack(side=TOP, anchor=NW)
+    count_label.pack(side=TOP, anchor=NW)
 
     y_scrollbar = Scrollbar(match_window)
     y_scrollbar.pack(side=RIGHT, fill=Y)
@@ -83,19 +76,25 @@ def search_button():
 
     listbox.pack(side=LEFT, expand=True, fill=BOTH)
 
-    
-    
-    
+    system_search2(listbox, regex)
+
+def hit_enter(event):
+    """allow the search button to be activated with enter key"""
+    search_button()
 
 root = Tk()
-root.title("System File Search")
+root.title('System File Search')
+root.geometry('300x30')
+root.resizable(False, False)
+
+root.bind('<Return>', hit_enter)
 
 label = Label(root, text='Filename')
-entry = Entry(root)
+entry = Entry(root, width=32)
 button = Button(root, text='Search', command=search_button)
 
-label.pack()
-entry.pack()
-button.pack()
+label.pack(side=LEFT)
+entry.pack(side=LEFT)
+button.pack(side=LEFT)
 
 root.mainloop()
